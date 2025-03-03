acc353a9735b856ba9b3896cdf3c34f3a3ceb1b3
NassimSimplon <nassimkhlifisimplonenda@gmail.com>
2025-03-03 13:23:02 +0100

    modify Animation

--- src/assets/CloseButtonIcon.jsx
+++ src/assets/CloseButtonIcon.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
 const CloseButtonIcon = () => {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 50 50" fill="#ffff">
-        <path d="M9.156 6.313 6.312 9.156 22.157 25 6.22 40.969 9.03 43.78 25 27.844 40.938 43.78l2.843-2.843L27.844 25 43.687 9.156l-2.843-2.844L25 22.157Z" />
-      </svg>
-    );
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="22"
+      height="22"
+      viewBox="0 0 50 50"
+      fill="#ffff"
+    >
+      <path d="M9.156 6.313 6.312 9.156 22.157 25 6.22 40.969 9.03 43.78 25 27.844 40.938 43.78l2.843-2.843L27.844 25 43.687 9.156l-2.843-2.844L25 22.157Z" />
+    </svg>
+  );
 };
-
-
- 
 
 export default CloseButtonIcon;
--- src/assets/PlusIcon.jsx
+++ src/assets/PlusIcon.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
- 
-
 const PlusIcon = () => {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="#2A266A">
-        <path d="M24 10H14V0h-4v10H0v4h10v10h4V14h10z" />
-      </svg>
-    );
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="17"
+      height="17"
+      viewBox="0 0 24 24"
+      fill="#2A266A"
+    >
+      <path d="M24 10H14V0h-4v10H0v4h10v10h4V14h10z" />
+    </svg>
+  );
 };
-
-
- 
 
 export default PlusIcon;
